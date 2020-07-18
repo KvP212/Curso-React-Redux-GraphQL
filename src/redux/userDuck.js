2ba3ea61f6724942a9b8695ccd5987d9bcdd388f
Kevin Pineda Perez <kevp212@icloud.com>
2020-07-18 17:51:29 -0600

    Create Action GoogleLogin

--- src/redux/userDuck.js
+++ src/redux/userDuck.js
@@ -1,17 +1,56 @@
+import { loginWithGoogle } from '../firebase'
 // constants
 let initialData = {
-  loggedIn: false
+  loggedIn: false,
+  fetching: false
 }
 
 let LOGIN = 'LOGIN'
+let LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+let LOGIN_ERROR = 'LOGIN_ERROR'
 
 //reducer
 export default function reducer(state = initialData, action) {
   switch (action.type) {
+    case LOGIN_SUCCESS:
+      return { ...state, fetching: false, error: ...action.payload }
+    
+    case LOGIN_ERROR:
+      return { ...state, fetching: false, error: action.payload }
+    
     case LOGIN:
+      return { ...state, fetching: true }
+    
     default:
       return state
   }
 }
 
+//function aux
+function saveStorage( storage ) {
+  localStorage.storage = JSON.stringify(storage)
+}
+
 //action (action creator)
+export let doGoogleLoginAction = () => (dispatch, getState) => {
+
+  dispatch({
+    type: LOGIN
+  })
+
+  return loginWithGoogle()
+    .then(user => {
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: { ...user }
+      })
+      saveStorage(getState())
+    })
+    .catch(e => {
+      console.log(e)
+      dispatch({
+        type: LOGIN_ERROR,
+        payload: e.message
+      })
+  })
+}
